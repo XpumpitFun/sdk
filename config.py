678b035a5ec70a1666b5a25a87765bc4be9ff89b
Xpumpit <epalokih@gmail.com>
2026-03-04 13:00:36 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+BOT_USERNAME = "XpumpitFun"
